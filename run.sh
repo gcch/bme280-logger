4694136db23570e8e79f494e24a8a2621658cc3b
tag <black.che.r.ry.karat.5@gmail.com>
2026-03-29 23:53:33 +0900

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,9 +1,11 @@
 #!/bin/bash
 
-if [ "$(uname)" = "Linux" ]; then
-    i2cdetect -y 1
-fi
-
 SCRIPT_DIR=$(cd $(dirname $0); pwd)
 cd "${SCRIPT_DIR}"
-$HOME/.local/bin/uv run "${SCRIPT_DIR}/main.py"
+
+if [ -f /proc/device-tree/model ] && grep -q "Raspberry Pi" /proc/device-tree/model; then
+    i2cdetect -y 1
+    $HOME/.local/bin/uv run "${SCRIPT_DIR}/main-i2c.py"
+else
+    $HOME/.local/bin/uv run "${SCRIPT_DIR}/main-ftdi.py"
+fi
